e0dbf44840a4c56a4af8c0d012b2b140e2ec8d29
AnaBenn <anastasia.n.bennett@gmail.com>
2017-06-02 02:17:03 +0000

    highest number cubed exercise

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -1,5 +1,18 @@
 """This is the entry point of the program."""
 
+#mine
+'''def highest_number_cubed(limit):
+    cubed = 0
+    for i in range(limit):
+        if i**3 >= limit:
+            cubed = i-1
+            return(cubed)'''
 
+#jason
 def highest_number_cubed(limit):
-    pass
+    number = 0
+    while True:
+        number += 1
+        if number**3 > limit:
+            return(number - 1)
+        
